35f3ef1b5e5c6a31bf710773ab9d8315ff87cb07
Ivan Stoickov <iv.stoickov@gmail.com>
2023-09-22 12:47:28 +0200

    Change backend word count;

--- backend/service.py
+++ backend/service.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 from bs4 import BeautifulSoup
@@ -7,5 +8,5 @@ def count_words_on_page(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     text = soup.get_text()
-    words = text.split()
+    words = re.findall("\w+", text)
     return len(words)
